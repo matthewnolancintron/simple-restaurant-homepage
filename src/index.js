013f7ac9ecd5570bfee3a64d7e724b26cc7ca518
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2021-12-05 06:30:46 -0500

    added a reminder to the comment with the useful advice

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
  * run npx webpack --watch
  * to not have to rerun webpack evertime
  * a change is made.
+ * 
+ * or 
+ * 
+ * run npx webpack to update a single time
  */
 
 console.log('is this working?');
