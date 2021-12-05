2c80efe12a0283bf6cf2b095dd12ad2f13ab7436
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2021-12-05 06:36:57 -0500

    commented a todo for dynamically adding markup to the index.html using multiple files and importing them for use inside index.js

--- src/index.js
+++ src/index.js
@@ -8,4 +8,19 @@
  * run npx webpack to update a single time
  */
 
+/**
+ * todo:
+ * create this markup dynamically with js
+ * <img src="cafe-1920.jpg" alt="possibly an illustration of cafe in greece during 1920?">
+        <h1>simple restaurant</h1>
+        <p>this is a wonderful restaurant</p>
+
+
+    and append it to div#content
+
+    going to write all the code in multiple files
+    and import and call it in here index.js
+ * 
+ */
+
 console.log('is this working?');
