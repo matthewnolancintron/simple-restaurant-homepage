04c995155b38a3690514dd56720d54de39259d27
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2021-12-10 07:04:31 -0500

    each new piece of content is placed before the last

--- src/page-load.js
+++ src/page-load.js
@@ -1,8 +1,7 @@
 function pageLoad(content){
     //append content to page when first is first loaded
     document.addEventListener('DOMContentLoaded',()=>{
-        let elementToAppendTo = document.getElementById('content');
-        elementToAppendTo.append(content);
+        document.body.prepend(content);
     });
 };
 
