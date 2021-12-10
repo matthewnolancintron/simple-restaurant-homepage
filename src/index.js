72a37265b764dc2ed083d8fcf0001d83b005d96e
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2021-12-10 09:05:19 -0500

    tab switching improved

--- src/index.js
+++ src/index.js
@@ -10,43 +10,46 @@
 import {pageLoad} from "./page-load.js";
 import {buildTabNavigation} from "./tab-navigation.js"
 import { buildHomeTabPage } from "./home-tab.js";
+import { buildMenuTabPage } from "./menu-tab.js";
 
 const tabNav = buildTabNavigation();
 const home = buildHomeTabPage();
-//const menu = 
+const menu = buildMenuTabPage();
 //const contacts = 
 
 //show default active tab page
 if(tabNav.firstElementChild.firstElementChild.classList = 'active-tab'){
-    console.log('true');
     let content = document.getElementById('content');
-    console.log(content);
     content.append(home);
 }
 
 for(var tab of tabNav.firstElementChild.children){
     tab.addEventListener('click',(e) => {
-        let selectedTab = e.target.textContent;
-        console.log(e.target.textContent,'index.js');
-        if(selectedTab == 'Home'){
-            /**
-             * if any content replace it with
-             * home content
-             * 
-             * if no content just place home
-             * content
-             * 
-             * use append
-             */
+        let content = document.getElementById('content');
+        
+        let isActiveTab = Boolean(e.target.classList);
+                
+        if(isActiveTab){
+            switch (e.target.textContent) {
+                case 'Home':
+                    //only add if it's not in the content div
+                    if(content.firstElementChild.classList != 'home_page'){
+                        //remove other page
+                        content.replaceChild(home,content.firstElementChild);
+                    }
+                    break;
+                case 'Menu':
+                    if(content.firstElementChild.classList != "menu_page"){
+                        content.replaceChild(menu,content.firstElementChild);
+                    }
+                    break;
+            
+                default:
+                    break;
+            }
+            //content.append()
         }
-
-        if(selectedTab == 'Menu'){
-
-        }
-
-        if(selectedTab == 'Contacts'){
-
-        }
+        
     });
 }
 
